e08b3c59b4fd4b30c93842909dcf8d93d81c69fb
felix <felix@zaslavskiy.net>
2023-04-04 00:09:06 -0400

    polish

--- src/practice/samples/PriorityMatrixTest.java
+++ src/practice/samples/PriorityMatrixTest.java
@@ -95,4 +95,111 @@ class PriorityMatrixTest {
         assertEquals(resource3, resourceMatrix.extractMax());
         assertEquals(resource4, resourceMatrix.getMax());
     }
+
+    @Test
+    void isEmpty() {
+        assertTrue(resourceMatrix.isEmpty());
+
+        resourceMatrix.insert(resource1, 1);
+        assertFalse(resourceMatrix.isEmpty());
+
+        resourceMatrix.remove(resource1);
+        assertTrue(resourceMatrix.isEmpty());
+    }
+
+    @Test
+    void size() {
+        assertEquals(0, resourceMatrix.size());
+
+        resourceMatrix.insert(resource1, 1);
+        assertEquals(1, resourceMatrix.size());
+
+        resourceMatrix.insert(resource2, 1);
+        assertEquals(2, resourceMatrix.size());
+
+        resourceMatrix.remove(resource1);
+        assertEquals(1, resourceMatrix.size());
+
+        resourceMatrix.remove(resource2);
+        assertEquals(0, resourceMatrix.size());
+    }
+
+    @Test
+    void updatePriority() {
+        resourceMatrix.insert(resource1, 1);
+        resourceMatrix.insert(resource2, 1);
+        resourceMatrix.insert(resource3, 2);
+        resourceMatrix.insert(resource4, 2);
+
+        resourceMatrix.updatePriority(resource1, 3);
+
+        assertEquals(resource2, resourceMatrix.getMin());
+        assertEquals(resource1, resourceMatrix.getMax());
+
+        resource4.availableResourcesRAM=10;
+        resourceMatrix.updatePriority(resource4, 1);
+        assertEquals(resource4, resourceMatrix.getMin()); // Corrected this line
+    }
+
+    @Test
+    void testRandomOperations() {
+        assertTrue(resourceMatrix.isEmpty());
+        assertEquals(0, resourceMatrix.size());
+
+        resourceMatrix.insert(resource1, 1);
+        resourceMatrix.insert(resource2, 1);
+        resourceMatrix.insert(resource3, 2);
+
+        assertFalse(resourceMatrix.isEmpty());
+        assertEquals(3, resourceMatrix.size());
+        assertEquals(resource1, resourceMatrix.getMin());
+        assertEquals(resource3, resourceMatrix.getMax());
+
+        resourceMatrix.updatePriority(resource2, 3);
+        resourceMatrix.insert(resource4, 1);
+
+        assertEquals(resource4, resourceMatrix.getMin());
+        assertEquals(resource2, resourceMatrix.getMax());
+
+        resourceMatrix.remove(resource4);
+        assertEquals(resource1, resourceMatrix.getMin());
+        assertEquals(3, resourceMatrix.size());
+
+        resourceMatrix.extractMin();
+        assertEquals(resource3, resourceMatrix.getMin());
+        assertEquals(2, resourceMatrix.size());
+
+        resourceMatrix.extractMax();
+        assertEquals(1, resourceMatrix.size());
+        assertEquals(resource3, resourceMatrix.getMin());
+    }
+    @Test
+    void testVariousOperations() {
+        assertTrue(resourceMatrix.isEmpty());
+
+        resourceMatrix.insert(resource1, 1);
+        resourceMatrix.insert(resource2, 1);
+        resourceMatrix.insert(resource3, 2);
+
+        assertEquals(resource1, resourceMatrix.getMin());
+
+        resourceMatrix.updatePriority(resource1, 3);
+        assertEquals(resource2, resourceMatrix.getMin());
+        assertEquals(resource1, resourceMatrix.getMax());
+
+        resourceMatrix.remove(resource2);
+        assertEquals(resource3, resourceMatrix.getMin());
+        assertEquals(2, resourceMatrix.size());
+
+        resourceMatrix.insert(resource4, 1);
+        assertEquals(resource4, resourceMatrix.getMin());
+        assertEquals(resource1, resourceMatrix.getMax());
+
+        resourceMatrix.extractMax();
+        assertEquals(resource3, resourceMatrix.getMax());
+
+        resourceMatrix.remove(resource4);
+        assertEquals(resource3, resourceMatrix.getMin());
+    }
+
 }
